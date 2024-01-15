b0aa466ce0194b58b53897ab1df0907d0158e511
Maeve Andersen <mv@ndr.sn>
2024-01-15 00:00:48 -0500

    chromedriver

--- nixos/home.nix
+++ nixos/home.nix
@@ -53,8 +53,9 @@
     gimp
     virt-manager
     libreoffice
-    # python (this sucks kinda, may wanna have flakes for dev enviros down the road)
+    # dev stuff (this sucks kinda, may wanna have flakes for dev enviros down the road)
     (python3.withPackages(ps: with ps; [ pandas requests selenium xlrd ]))
+    chromedriver
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
